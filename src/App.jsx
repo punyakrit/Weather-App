dda4f2de333c95b6e95677ddb3866a53b77e60e1
Punyakrit <punyakritsinghmakhni.com>
2024-01-18 20:39:58 +0530

    top-buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from "react";
+import UilReact from "@iconscout/react-unicons/icons/uil-react";
+import TopButton from "./components/TopButton";
+import Input from "./components/Input";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400 rounded-lg">
+      <TopButton />
+      <Input />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
